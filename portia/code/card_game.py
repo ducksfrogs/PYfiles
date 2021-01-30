79b536b97f25011dcc34b1d33f40719e4fb3fb1a
ducksfrogs <ma_yamaki@yahoo.com>
2021-01-30 11:55:25 +0900

    d

--- portia/code/card_game.py
+++ portia/code/card_game.py
@@ -65,4 +65,78 @@ class Player():
 
     def __str__(self):
         return f'Player {self.name} has {len(self.all_cards)} cards.'
+
+
+#Game logic part
+player_one = Player('One')
+player_two = Player("Two")
+
+new_deck = Deck()
+new_deck.shuffle()
+
+for x in range(26):
+    player_one.add_cards(new_deck.deal_one())
+    player_two.add_cards(new_deck.deal_one())
+
+game_on = True
+
+round_num = 0
+
+while game_on:
+    round_num += 1
+    print(f"Round {round_num}")
+
+    if len(player_one.all_cards) ==0:
+        print("Player One, out of cards! Player Two Wins")
+        game_on = False
+        break
+
+    if len(player_two.all_cards) ==0:
+        print("Player Two, out of cards! Player One Wins")
+        game_on = False
+        break
+
+    #Start A New
+    player_one_cards = []
+    player_one_cards.append(player_one.remove_one())
+    player_two_cards = []
+    player_two_cards.append(player_two.remove_one())
+
+    at_war = True
+
+    while at_war:
+        if player_one_cards[-1].value > player_two_cards[-1].value:
+            player_one.add_cards(player_one_cards)
+            player_one.add_cards(player_two_cards)
+
+            at_war = False
+        elif player_one_cards[-1].value < player_two_cards[-1].value:
+            player_two.add_cards(player_one_cards)
+            player_two.add_cards(player_two_cards)
+
+            at_war = False
+
+        else:
+            print("WAR!")
+
+            if len(player_one.all_cards) < 3:
+                print("Player One Unable to declare War")
+                print("Player Two Wins")
+                game_on = False
+
+            elif len(player_two.all_cards) < 3:
+                print("Player Two Unable to declare War")
+                print("Player One Wins")
+                game_on = False
+
+            else:
+                for num in range(3):
+                    player_one_cards.append(player_one.remove_one())
+                    player_two_cards.append(player_two.remove_one())
+
+
+
+
+
+
     
