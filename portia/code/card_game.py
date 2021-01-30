bab61c2084e37d762b1688b33a7a49f784541232
ducksfrogs <ma_yamaki@yahoo.com>
2021-01-30 11:08:11 +0900

    d

--- portia/code/card_game.py
+++ portia/code/card_game.py
@@ -30,7 +30,6 @@ class Deck():
         for suit in suits:
             for rank in ranks:
                 created_card = Card(suit, rank)
-
                 self.all_cards.append(created_card)
 
     def shuffle(self):
@@ -38,8 +37,32 @@ class Deck():
         random.shuffle(self.all_cards)
 
 
+    def deal_one(self):
+        return self.all_cards.pop()
+
+
 new_deck = Deck()
 
 for card_object in new_deck.all_cards:
     print(card_object)
 #Player
+
+class Player():
+    def __init__(self, name):
+
+        self.name = name
+        self.all_cards = []
+
+    def remove_one(self):
+        return self.all_cards.pop(0)
+
+    def add_cards(self, new_cards):
+        if type(new_cards) == type([]):
+            self.all_cards.extend(new_cards)
+        else:
+            self.all_cards.append(new_cards)
+
+
+    def __str__(self):
+        return f'Player {self.name} has {len(self.all_cards)} cards.'
+    
