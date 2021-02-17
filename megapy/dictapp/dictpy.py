20b8431e0c87fe54dfe0dd48b08007aa53649bdd
ma <ma_yamaki@yahoo.com>
2021-02-18 07:27:16 +0900

    dd

---
+++ megapy/dictapp/dictpy.py
@@ -0,0 +1,24 @@
+import json
+from difflib import SequenceMatcher
+from difflib import get_close_matches
+
+
+
+data = json.load(open('data.json'))
+
+def translate(word):
+    word = word.lower()
+
+    if word in data:
+        return data[word]
+    elif len(get_close_matches(word, data.keys()))>0:
+        yn = input("did you mean %s instead? Enter Y if yes" % get_close_matches(word, data.keys())[0])
+        if yn == "y":
+            return data[get_close_matches(word, data.keys())[0]]
+    else:
+        return "The word does not exist"
+
+word = input("Enter a word ")
+
+
+print(translate(word))
